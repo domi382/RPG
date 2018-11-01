06686d922babc67050f2005ea49410812c8193e2
domi382 <3392634+domi382@users.noreply.github.com>
2018-11-01 19:38:02 +0100

    started working on threads

--- src/de/domi382/rpg/main/Game.java
+++ src/de/domi382/rpg/main/Game.java
@@ -2,27 +2,53 @@ package de.domi382.rpg.main;
 
 import java.awt.Canvas;
 import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
+import de.domi382.rpg.states.GameState;
+import de.domi382.rpg.states.MenuState;
+import de.domi382.rpg.states.PauseState;
 import de.domi382.rpg.states.StateManager;
 
-public class Game {
+public class Game implements Runnable{
+	private Thread t;
+	public BufferStrategy bs;
+	public Graphics g;
 	StateManager sm = new StateManager();
 	public Boolean isRunning = true;
+	public Canvas canvas = new Canvas();
+	GameState gs = new GameState();
+	PauseState ps = new PauseState();
+	MenuState ms = new MenuState();
+	
 	public Game(Canvas canvas, JFrame window) {
+		sm.setState(gs); 
+		canvas.createBufferStrategy(3);
+	}
+	
+	public void init() {
 		
 	}
 	
 	public void run() {
 		if (isRunning) {
-			render();
+			render(g);
 			tick();
 		}
 	}
 	
-	public void render() {
+	public void start () {
+	      if (t == null) {
+	         t = new Thread (this, "Game");
+	         t.start ();
+	      }
+	   }
+	
+	public void render(Graphics g) {
 		if(sm.getState() != null) sm.getState().render(null);
+		bs = canvas.getBufferStrategy();
+        g = bs.getDrawGraphics();
 	}
 	
 	public void tick() {
